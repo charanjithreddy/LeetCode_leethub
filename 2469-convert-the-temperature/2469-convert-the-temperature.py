8819234e5d0a7174259adde30c6424b7b503d1b5
charanjithreddy <charanjith.angadi@gmail.com>
2024-07-10 22:39:45 +0530

    Time: 14 ms (51.13%), Space: 11.6 MB (55.58%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        return [273.15+celsius,32+celsius*1.8];
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        
