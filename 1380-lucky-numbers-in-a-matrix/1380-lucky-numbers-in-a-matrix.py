a1f42ed37ac1a6077afb1e3eaab7e495554aafe3
charanjithreddy <charanjith.angadi@gmail.com>
2024-07-19 07:43:42 +0530

    Time: 105 ms (20.47%), Space: 11.9 MB (22.05%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def luckyNumbers (self, matrix):
+        o=[];
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if(matrix[i][j]==min(matrix[i])):
+                    s=[];
+                    for k in range(len(matrix)):
+                        s.append(matrix[k][j]);
+                    if(matrix[i][j]==max(s)):
+                        o.append(matrix[i][j]);
+        return o;
+
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        
