6078b48efd9a3b1ee11455bfc38168fb005478f8
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-03 11:16:29 +0530

    Time: 19 ms (69.10%) | Memory: 14.7 MB (50.35%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def candy(self, ratings):
+        o=[1]*len(ratings);
+        for i in range(1,len(ratings)):
+            if(ratings[i]>ratings[i-1]):
+                o[i]=o[i-1]+1;
+        for i in range(len(ratings)-2,-1,-1):
+            if(ratings[i]>ratings[i+1]):
+                if(o[i]<=o[i+1]):
+                    o[i]=o[i+1]+1;
+        return sum(o);
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
