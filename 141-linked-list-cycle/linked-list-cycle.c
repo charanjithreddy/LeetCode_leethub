100cf0cea99f1ab7dc23e3ba179f304f56eb772f
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-04 22:17:37 +0530

    Time: 9 ms (78.43%) | Memory: 11.3 MB (38.47%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.c
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -6,13 +6,11 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    if(head==NULL || head->next==NULL){
+    if(head==NULL||head->next==NULL){
         return false;
     }
-    struct ListNode* slow=head;
-    struct ListNode* fast=head;
-    while(fast!=NULL&&fast->next!=NULL){
-        
+    struct ListNode *slow=head,*fast=head;
+    while(fast!=NULL&&fast->next!=NULL){ 
         slow=slow->next;
         fast=fast->next->next;
         if(slow==fast){
