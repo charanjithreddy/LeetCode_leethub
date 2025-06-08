9b3eac462387dcf03cd6c7bccb070aa3d8ba8145
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-08 18:11:38 +0530

    Time: 34 ms (45.54%) | Memory: 18.7 MB (80.36%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def lexicalOrder(self, n):
-        l=sorted([str(i) for i in range(1,n+1)]);
-        return [int(i) for i in l]
+        return [int(i) for i in sorted([str(i) for i in range(1,n+1)])]
         """
         :type n: int
         :rtype: List[int]
