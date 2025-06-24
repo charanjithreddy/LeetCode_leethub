e34b45cb6defdd84aced926e7dc9a31ea9d8d79f
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-24 12:39:09 +0530

    Time: 2 ms (89.58%) | Memory: 12.7 MB (29.17%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -4,14 +4,24 @@ class Solution(object):
         l=len(nums);
         for i in range(l):
             if(nums[i]==key):
-                o.append(i);
-                x=max(0,i-k);
-                for j in range(x,i):
-                    o.append(j);
+                if(len(o)==0):
+                    x=max(0,i-k);
+                    if(x<i):
+                        for j in range(x,i):
+                            o.append(j);
+                else:
+                    x=max(0,i-k,o[-1]+1);
+                    if(x<i):
+                        for j in range(x,i):
+                            o.append(j);
+                if(len(o)==0):
+                    o.append(i);
+                elif(i>o[-1]):
+                    o.append(i);
                 x=min(l-1,i+k);
-                for j in range(i+1,x+1):
+                for j in range(max(i+1,o[-1]+1),x+1):
                     o.append(j);
-        return sorted(list(set(o)));
+        return o;
                 
 
         """
