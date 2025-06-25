c143e6053ec648659bed13e13c075c0e5d24d657
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-25 11:22:41 +0530

    Time: 8 ms (96.14%) | Memory: 12.7 MB (54.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,12 +2,27 @@ class Solution(object):
     def isAnagram(self, s, t):
         if(len(s)!=len(t)):
             return False;
-        for i in list(set(t)):
-            if(i not in s):
+        sh={};
+        th={};
+        for i in s:
+            if(i not in sh):
+                sh[i]=1;
+            else:
+                sh[i]+=1;
+        for i in t:
+            if(i not in th):
+                th[i]=1;
+            else:
+                th[i]+=1;
+        if(len(sh)!=len(th)):
+            return False;
+        for i in sh:
+            if(i not in th or sh[i]!=th[i]):
                 return False;
-            elif(t.count(i)!=s.count(i)):
+        for i in th:
+            if(i not in sh or sh[i]!=th[i]):
                 return False;
-        return True;        
+        return True;
         """
         :type s: str
         :type t: str
