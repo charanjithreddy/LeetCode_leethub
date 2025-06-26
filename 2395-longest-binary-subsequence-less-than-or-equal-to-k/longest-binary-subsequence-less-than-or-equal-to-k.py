024f8e6a1dd8923ee7554c1a48acb92900a6a988
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-26 15:58:58 +0530

    Time: 87 ms (18.18%) | Memory: 12.4 MB (36.36%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -3,11 +3,12 @@ class Solution(object):
         l=len(s);
         v=0;
         o=0;
-        for i in range(len(s)-1,-1,-1):
-            if(v+int(s[i])*(2**(l-i-1))<=k):
-                v+=int(s[i])*(2**(l-i-1));
+        s=[int(i) for i in s];
+        for i in range(l-1,-1,-1):
+            if(v+(s[i])*(2**(l-i-1))<=k):
+                v+=(s[i])*(2**(l-i-1));
                 o+=1;
-            elif(s[i]=="0"):
+            elif(s[i]==0):
                 o+=1;
         return o;
             
