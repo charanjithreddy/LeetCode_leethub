a15f0cb9616355ba5e7fdbb512de543be5746b9d
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-27 11:43:48 +0530

    Time: 32 ms (28.34%) | Memory: 15.9 MB (98.64%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        d={};
+        for i in strs:
+            if("".join(sorted(i)) in d):
+                d["".join(sorted(i))].append(i);
+            else:
+                d["".join(sorted(i))]=[i];
+        return [d[i] for i in d];
+
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
