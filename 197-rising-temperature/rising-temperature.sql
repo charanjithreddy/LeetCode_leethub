3866204d39dde87f446c72e94f69fd468506947c
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-30 21:58:46 +0530

    Time: 447 ms (74.97%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-select t1.id from Weather t1,Weather t2 where t1.temperature>t2.temperature and 
-
-DATE_SUB(t1.recordDate,INTERVAL 1 DAY)=t2.recordDate;
+select t1.id from Weather t1,Weather t2 where t1.temperature>t2.temperature and DATE_SUB(t1.recordDate,INTERVAL 1 DAY)=t2.recordDate;
