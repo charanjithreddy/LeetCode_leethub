871bbb7e12a375bcae5432c63c0e469ace2200be
charanjithreddy <charanjith.angadi@gmail.com>
2025-06-30 10:54:13 +0530

    Time: 42 ms (55.37%) | Memory: 52.6 MB (94.02%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+   let o=[];
+   let x=0;
+   for(let i=0;i<arr.length;i++){
+    if(fn(arr[i],i)){
+        o[x]=arr[i];
+        x++;
+    }
+   } 
+   return o;
+};
