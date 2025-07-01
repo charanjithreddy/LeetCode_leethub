e5e5b2e35652eae1928f398ea8dca320286e7f1a
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-01 11:50:24 +0530

    Time: 22 ms (25.71%) | Memory: 12.4 MB (88.57%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def possibleStringCount(self, word):
+        o=1;
+        curr=word[0];
+        t=0;
+        for i in word:
+            if(i==curr):
+                t+=1;
+            else:
+                o+=(t-1);
+                t=1;
+                curr=i;
+        o+=t-1;
+
+        return o;
+        """
+        :type word: str
+        :rtype: int
+        """
+        
