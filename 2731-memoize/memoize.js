f7a521c5452a82dea482294db7f2280a46d5ee53
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-02 12:25:52 +0530

    Time: 350 ms (5.06%) | Memory: 93.6 MB (85.40%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    cache={}
+    return function(...args) {
+        if(args in cache){
+            return cache[args];
+        }
+        else{
+            cache[args]=fn(...args);
+            return cache[args];
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
