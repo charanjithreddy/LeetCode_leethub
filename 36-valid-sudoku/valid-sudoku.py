1adee465d0f44be5abffe2ad989c779b39d6328d
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-02 12:17:58 +0530

    Time: 4 ms (83.10%) | Memory: 12.6 MB (16.00%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,42 +1,22 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        for i in range(9):
-            for j in range(9):
-                if(board[i][j]=="."):
-                    board[i][j]=10;
-                else:
-                    board[i][j]=int(board[i][j]);
-        for  i in board:
-            a=[0]*10;
-            for j in i:
-                if(j<10):
-                    a[j]+=1;
-                    if(a[j]>1):
-                        return False;
-        for i in range(len(board)):
-            x=[];
-            for j in range(len(board)):
-                x.append(board[j][i]);
-            a=[0]*10;
-            for j in x:
-                if(j<10):
-                    a[j]+=1;
-                    if(a[j]>1):
-                        return False;
-        for i in range(3):
-            for j in range(3):
-                x=[];
-                for r in range(3*i,(i+1)*3):
-                    for c in range(3*j,(j+1)*3):
-                        x.append(board[r][c]);
-                a=[0]*10;
-                for ele in x:
-                    if(ele<10):
-                        a[ele]+=1;
-                        if(a[ele]>1):
-                            return False;             
+        rows=[[] for i in range(9)];
+        cols=[[] for i in range(9)];
+        box=[[[],[],[]],[[],[],[]],[[],[],[]]];
+        for r in range(9):
+            for c in range(9):
+                if(board[r][c]=="."):
+                    continue;
+                if(board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in box[r//3][c//3]):
+                    return False;
+                rows[r].append(board[r][c]);
+                cols[c].append(board[r][c]);
+                box[r//3][c//3].append(board[r][c]);
         return True;
+
+
         """
         :type board: List[List[str]]
         :rtype: bool
         """
+        
