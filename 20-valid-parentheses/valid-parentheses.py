c26479ed44fe9adf29af18edc1311d4c866b4fe9
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-04 13:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (20.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[];
+        l=0;
+        for i in s:
+            if(i=='(' or i=='{' or i=='['):
+                stack.append(i);
+                l+=1;
+            else:
+                if(i==')' and l>0 and stack[l-1]=='('):
+                    stack.pop(l-1);
+                    l-=1;
+                elif(i==']' and l>0  and stack[l-1]=='['):
+                    stack.pop(l-1);
+                    l-=1;
+                elif(i=='}' and l>0  and stack[l-1]=='{'):
+                    stack.pop(l-1);
+                    l-=1;
+                else:
+                    return False;
+        return len(stack)==0;
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
