aab922f259870055934806eb09ef1266526e84e3
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-05 12:56:55 +0530

    Time: 19 ms (44.95%) | Memory: 15.2 MB (6.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,7 @@
 class Solution(object):
     def isPalindrome(self, s):
-        s=list(s);
-        s=''.join([lower(i) for i in s if i.isalnum()]);
+        s=''.join([lower(i) for i in list(s) if i.isalnum()]);
         return s==s[::-1];
-        s1="";
-        for i in s:
-            if(i.isalnum()):
-                s1+=i;
-        s1=lower(s1);
-        return s1==s1[::-1];
         """
         :type s: str
         :rtype: bool
