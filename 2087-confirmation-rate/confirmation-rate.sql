65f360e928b92b315681294c2755173b42b72202
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-06 22:25:42 +0530

    Time: 657 ms (56.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select s.user_id,ifnull(round(count(case when action="confirmed" then action end)/count(action),2),0) as confirmation_rate from Signups s left join Confirmations c on s.user_id=c.user_id group by s.user_id;
