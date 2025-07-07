ea40ad4c51d673cf0d7d89d4683f9003873f606a
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-07 21:43:49 +0530

    Time: 9 ms (53.22%) | Memory: 13.8 MB (32.19%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        o=[];
+        for i in tokens:
+            if(i=="+"):
+                res=int(o.pop())+int(o.pop());
+                o.append(res);
+            elif(i=="-"):
+                res=int(o.pop());
+                res=int(o.pop())-res;
+                o.append(res);
+            elif(i=="*"):
+                res=int(o.pop())*int(o.pop());
+                o.append(res);
+            elif(i=="/"):
+                res=float(o.pop());
+                res=int(o.pop())/res;
+                o.append(int(res));
+            else:
+                o.append(int(i));
+        return o[0];
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
