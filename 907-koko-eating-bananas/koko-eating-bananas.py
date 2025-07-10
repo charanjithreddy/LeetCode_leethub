08b6a4439e9bdcf78fb0472959d2d4328f9e3347
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-10 21:22:27 +0530

    Time: 216 ms (62.46%) | Memory: 13.6 MB (9.49%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,29 @@
+import math;
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        left=1;
+        right=max(piles);
+        res=right;
+        while(left<=right):
+            mid=(left+right)//2;
+            o=0;
+            for i in piles:
+                if(i<mid):
+                    o+=1;
+                else:
+                    if(i%mid==0):
+                        o+=i//mid;
+                    else:
+                        o+=i//mid+1;
+            if(o<=h):
+                res=min(mid,res);
+                right=mid-1;
+            else:
+                left=mid+1;
+        return res;
+
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
