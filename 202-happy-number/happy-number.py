ef16617b6907de64d729a3cae8402017ada51db5
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-12 09:47:13 +0530

    Time: 4 ms (46.63%) | Memory: 12.5 MB (50.52%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def isHappy(self, n):
-        while(n>6):
+        s=set();
+        while(n>=1):
+            if(n==1):
+                return True;
             t=0;
             while(n>0):
                 t+=(n%10)**2;
                 n//=10;
+            if(t in s):
+                return False;
+            s.add(t);
             n=t;
         return n==1;
         """
