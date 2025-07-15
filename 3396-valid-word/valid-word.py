3f2f37da81ea6503d648787429534151dce2eb97
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-15 19:00:28 +0530

    Time: 4 ms (17.35%) | Memory: 12.5 MB (8.16%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, word):
+        vowels=['a','e','i','o','u','A','E','I','O','U'];
+        v=0;
+        c=0;
+        if(len(word)<3 or not(word.isalnum())):
+            return False;
+        for i in word:
+            if(i in vowels):
+                v+=1;
+            elif(i.isalpha()):
+                c+=1;
+        return v>=1 and c>=1;
+
+        """
+        :type word: str
+        :rtype: bool
+        """
+        
