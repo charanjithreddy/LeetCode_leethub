b066b19e62e8d721964fe71018ee25cdbd6af985
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-16 09:53:06 +0530

    Time: 883 ms (68.44%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select st.student_id,st.student_name,su.subject_name,ifnull(count(ex.subject_name),0) as attended_exams from Students st cross join Subjects su left join examinations ex on st.student_id=ex.student_id and su.subject_name=ex.subject_name group by st.student_id,su.subject_name order by st.student_id,su.subject_name
+select st.student_id,st.student_name,su.subject_name,ifnull(count(ex.subject_name),0) as attended_exams from Students st cross join Subjects su left join examinations ex on st.student_id=ex.student_id and su.subject_name=ex.subject_name group by st.student_id,su.subject_name order by st.student_id,su.subject_name;
