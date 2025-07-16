651c7dfff3070899b34bbc749353f2bd49c77f97
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-16 09:21:50 +0530

    Time: 81 ms (54.55%) | Memory: 24.7 MB (45.45%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -2,8 +2,6 @@ class Solution(object):
     def maximumLength(self, nums):
         o=0;
         e=0;
-        oe=[];
-        eo=[];
         comb=[];
         for i in nums:
             if(i%2==0):
@@ -12,20 +10,13 @@ class Solution(object):
                     comb.append(i);
                 elif(comb[-1]%2==1):
                     comb.append(i);
-                if(len(eo)==0 or eo[-1]%2==1):
-                    eo.append(i);
-                if(len(oe)>0 and oe[-1]%2==1):
-                    oe.append(i);
+                
             else:
                 o+=1;
                 if(len(comb)==0):
                     comb.append(i);
                 elif(comb[-1]%2==0):
                     comb.append(i);
-                if(len(oe)==0 or oe[-1]%2==0):
-                    oe.append(i);
-                if(len(eo)>0 and eo[-1]%2==0):
-                    eo.append(i);
         return max(o,e,len(comb));
         """
         :type nums: List[int]
