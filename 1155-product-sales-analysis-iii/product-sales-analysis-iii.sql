7f65aa230205b1c409af97489ec5a8fa61488346
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-18 09:28:05 +0530

    Time: 743 ms (49.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select s1.product_id,s1.year as first_year,s1.quantity,s1.price from Sales s1 left join Sales s2 on s1.product_id=s2.product_id and s1.year>s2.year where s2.product_id is null;
