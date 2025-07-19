e2db2d4a28c7ddad1c07319096f0ec29df1cdaae
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-19 19:19:19 +0530

    Time: 35 ms (65.41%) | Memory: 21 MB (57.58%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def findDuplicate(self, nums):
         for i in nums:
-            if(nums[abs(i)]<0):
-                return abs(i);
-            nums[abs(i)]*=-1;
+            x=abs(i);
+            if(nums[x]<0):
+                return x;
+            nums[x]*=-1;
         """
         :type nums: List[int]
         :rtype: int
