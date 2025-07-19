9a54c6880ba3903c548485a3973e86b54ff10b85
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-19 09:30:47 +0530

    Time: 934 ms (6.67%) | Memory: 24 MB (100.00%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -2,7 +2,14 @@ class Solution(object):
     def removeSubfolders(self, folder):
         folder.sort(key=lambda x:x.count('/'))
         o=set();
-        for i in folder:
+        for i in range(len(folder)):
+            j=i+1;
+            while(j<len(folder)):
+                if(folder[i]+'/'in folder[j] and folder[j].index(folder[i]+'/')==0):
+                    folder.pop(j);
+                else:
+                    j+=1;
+            """
             x=1;
             for j in o:
                 s=j+'/';
@@ -10,8 +17,8 @@ class Solution(object):
                     x=0;
                     break;
             if(x==1):
-                o.add(i);
-        return list(o);
+                o.add(i);"""
+        return folder;
         """
         :type folder: List[str]
         :rtype: List[str]
