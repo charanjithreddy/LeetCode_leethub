c34e6bd4bbcb8e4b9ec08f0ac9c283fddf79f58f
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-24 17:31:44 +0530

    Time: 1650 ms (31.81%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -4,4 +4,4 @@ select 'Low Salary' as category
 union all
 select 'Average Salary'
 union all
-select 'High Salary' ) as t1 left join (select account_id,(case when income<20000 then 'Low Salary' when income between 20000 and 50000 then 'Average Salary' else 'High Salary' end) as category from Accounts) as t2 on t1.category=t2.category group by t1.category;
+select 'High Salary' ) as t1 left join (select account_id,(case when income<20000 then 'Low Salary' when income <=50000 then 'Average Salary' else 'High Salary' end) as category from Accounts) as t2 on t1.category=t2.category group by t1.category;
