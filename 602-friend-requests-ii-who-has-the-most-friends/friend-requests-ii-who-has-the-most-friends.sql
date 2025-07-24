59850fa9930f87107dc972510f59ac4e9acfab60
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-24 21:11:09 +0530

    Time: 309 ms (51.60%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-select * from (select f1 as id,count(f2) as num from (select requester_id as f1,accepter_id as f2 from RequestAccepted
+select f1 as id,count(f2) as num from (select requester_id as f1,accepter_id as f2 from RequestAccepted
 union all
-select accepter_id as f1,requester_id as f2 from RequestAccepted) as t group by f1) as t1 
-where num=(select max(num) from (select f1 as id,count(f2) as num from (select requester_id as f1,accepter_id as f2 from RequestAccepted
-union all
-select accepter_id as f1,requester_id as f2 from RequestAccepted) as t group by f1) as t2)
+select accepter_id as f1,requester_id as f2 from RequestAccepted) as t group by f1 order by num desc limit 1;
