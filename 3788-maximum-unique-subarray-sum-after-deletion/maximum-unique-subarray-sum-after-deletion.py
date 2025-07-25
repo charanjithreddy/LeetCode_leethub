a0eda580c2e5a571399b66fa9de344a0db2e0b34
charanjithreddy <charanjith.angadi@gmail.com>
2025-07-25 10:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (56.45%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -3,9 +3,7 @@ class Solution(object):
         if(max(nums)<0):
             return max(nums)
         return sum(set([i for i in nums if i>0]));
-
         """
         :type nums: List[int]
         :rtype: int
         """
-        
