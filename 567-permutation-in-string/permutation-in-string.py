3485db1278fd831fe484f363c290a3aa6908d92b
charanjithreddy <charanjith.angadi@gmail.com>
2025-08-07 09:31:00 +0530

    Time: 167 ms (25.66%) | Memory: 12.8 MB (36.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,24 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
+        if(len(s1)>len(s2)):
+            return False;
+        a=[0]*26;
+        for i in s1:
+            a[ord(i)-ord('a')]+=1;
+        a=''.join([str(i) for i in a]);
+        t=[0]*26;
+        for i in range(len(s1)):
+            t[ord(s2[i])-ord('a')]+=1;
         i=0;
-        j=len(s1);
-        s=sorted(list(s1));
-        while(j<=len(s2)):
-            x=list(s2[i:j]);
-            if(sorted(x)==s):
+        j=len(s1)-1;
+        while(j<len(s2)):
+            if(a==''.join([str(x) for x in t])):
                 return True;
+            t[ord(s2[i])-ord('a')]-=1;
             i+=1;
             j+=1;
+            if(j<len(s2)):
+                t[ord(s2[j])-ord('a')]+=1;
         return False;
         """
         :type s1: str
