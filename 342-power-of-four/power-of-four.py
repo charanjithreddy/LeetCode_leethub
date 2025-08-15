54088ba777fcaf78e87e231501b92debba37b582
charanjithreddy <charanjith.angadi@gmail.com>
2025-08-15 10:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.82%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,8 +1,10 @@
 class Solution(object):
     def isPowerOfFour(self, n):
         while(n>0):
-            if(n==1):
+            if(n==1 or n==4):
                 return True;
+            elif((n%100)%4!=0):
+                return False;
             n/=4.0;
         return False;
         """
