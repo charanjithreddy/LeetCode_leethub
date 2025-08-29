6161e84f4ec672c4978d1b38ebc05ee2b85358b3
charanjithreddy <charanjith.angadi@gmail.com>
2025-08-29 09:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (81.25%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,15 +1,8 @@
 class Solution(object):
     def flowerGame(self, n, m):
-        n_e=n//2;
-        n_o=n-n_e;
-        m_e=m//2;
-        m_o=m-m_e;
-        return n_e*m_o+n_o*m_e;
-
-
+        return (n//2)*(m-(m//2))+(n-(n//2))*(m//2);
         """
         :type n: int
         :type m: int
         :rtype: int
         """
-        
