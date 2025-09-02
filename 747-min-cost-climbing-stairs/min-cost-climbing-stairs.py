3e206cc2253a05c2f32a94e2949002b0ef07db25
charanjithreddy <charanjith.angadi@gmail.com>
2025-09-02 09:43:29 +0530

    Time: 1 ms (82.99%) | Memory: 12.6 MB (27.31%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        a=[0]*(len(cost)+1);
+        a[0]=0;
+        a[1]=0;
+        for i in range(2,len(a)):
+            a[i]=min(a[i-1]+cost[i-1],a[i-2]+cost[i-2]);
+        return a[-1];
+
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        
