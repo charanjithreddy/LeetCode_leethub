1273bc714358e76f0bdce2e03ab7b4c9b05dcbb2
charanjithreddy <charanjith.angadi@gmail.com>
2025-09-10 09:28:03 +0530

    Time: 594 ms (28.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select request_at as Day,round(count(case when status!='completed' then 1 else null end)/count(*),2) as `Cancellation Rate` from trips t left join users u1 on t.client_id=u1.users_id left join users u2 on  t.driver_id=u2.users_id where u1.banned='No' and u2.banned='No' and (request_at between str_to_date("2013-10-01","%Y-%m-%d") and str_to_date("2013-10-03","%Y-%m-%d")) group by request_at;
