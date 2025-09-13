d24b91221d160bbf112d05a3c6761e618effff7c
charanjithreddy <charanjith.angadi@gmail.com>
2025-09-13 10:14:06 +0530

    Time: 3 ms (80.90%) | Memory: 12.4 MB (53.86%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,24 +1,23 @@
 class Solution(object):
     def maxFreqSum(self, s):
+        d={};
+        for i in s:
+            if(i in d):
+                d[i]+=1;
+            else:
+                d[i]=1;
         vowels="aeiou";
         dv={};
         dv['A']=0;
         dc={};
         dc['B']=0;
-        for i in s:
+        for i in d:
             if(i in vowels):
-                if(i in dv):
-                    dv[i]+=1;
-                else:
-                    dv[i]=1;
+                dv[i]=d[i];
             else:
-                if(i in dc):
-                    dc[i]+=1;
-                else:
-                    dc[i]=1;
-        return max(list(dv.values()))+max(list(dc.values()));
+                dc[i]=d[i];
+        return max(list(dv.values()))+max(list(dc.values()));        
         """
         :type s: str
         :rtype: int
         """
-        
