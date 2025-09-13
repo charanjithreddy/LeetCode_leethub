78712a0ad31d1e7f61079f8551ded929db5a7518
charanjithreddy <charanjith.angadi@gmail.com>
2025-09-13 10:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (76.81%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -5,8 +5,10 @@ class Solution(object):
             if(i[1]<newInterval[0] or i[0]>newInterval[1]):
                 o.append(i);
             else:
-                newInterval[0]=min(i[0],newInterval[0]);
-                newInterval[1]=max(i[1],newInterval[1]);
+                if(i[0]<newInterval[0]):
+                    newInterval[0]=i[0];
+                if(i[1]>newInterval[1]):
+                    newInterval[1]=i[1];
         o.append(newInterval);
         o=sorted(o,key=lambda x:x[0]);
         return o;
