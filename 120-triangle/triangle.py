2828facef92e8dba1dd53dbd910d29d10ff27981
charanjithreddy <charanjith.angadi@gmail.com>
2025-09-25 09:57:17 +0530

    Time: 3 ms (86.40%) | Memory: 13.1 MB (53.34%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                triangle[i][j]=min(triangle[i][j]+triangle[i+1][j],triangle[i][j]+triangle[i+1][j+1]);
+        return triangle[0][0];
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        
