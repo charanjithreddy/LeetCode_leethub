629d3ef427347a1e4eef672125b784378d14a9fc
charanjithreddy <charanjith.angadi@gmail.com>
2025-10-01 10:37:14 +0530

    Time: 25 ms (83.74%) | Memory: 15.2 MB (38.42%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        res=[0];
+        m=len(grid);
+        n=len(grid[0]);
+        visited=[[0 for i in range(n)]for i in range(m)];
+
+        def func(i,j):
+            visited[i][j]=1;
+            if(i+1<m and grid[i+1][j]==1 and visited[i+1][j]==0):
+                res[-1]+=1;
+                func(i+1,j);
+            if(j+1<n and grid[i][j+1]==1 and visited[i][j+1]==0):
+                res[-1]+=1;
+                func(i,j+1);
+            if(i-1>=0 and grid[i-1][j]==1 and visited[i-1][j]==0):
+                res[-1]+=1;
+                func(i-1,j);
+            if(j-1>=0 and grid[i][j-1]==1 and visited[i][j-1]==0):
+                res[-1]+=1;
+                func(i,j-1);
+        for i in range(m):
+            for j in range(n):
+                if(grid[i][j]==1 and visited[i][j]==0):
+                    res.append(1);
+                    func(i,j);
+        return max(res);
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
