6e52472d0b286791e01d8ec093cfc6a918d98574
charanjithreddy <charanjith.angadi@gmail.com>
2025-10-02 09:48:33 +0530

    Time: 257 ms (90.46%) | Memory: 0B (100.00%) - LeetSync

--- 3816-dna-pattern-recognition/dna-pattern-recognition.sql
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-select *,
-case when dna_sequence regexp '^ATG' then 1 else 0 end as has_start,
-case when dna_sequence regexp 'TAA$' or dna_sequence regexp 'TAG$' or dna_sequence regexp 'TGA$' then 1 else 0 end as has_stop,
-case when dna_sequence regexp 'ATAT' then 1 else 0 end as has_atat,
-case when dna_sequence regexp 'GGG' then 1 else 0 end as has_ggg
-from samples;
+select *,case when dna_sequence regexp '^ATG' then 1 else 0 end as has_start,case when dna_sequence regexp 'TAA$' or dna_sequence regexp 'TAG$' or dna_sequence regexp 'TGA$' then 1 else 0 end as has_stop,case when dna_sequence regexp 'ATAT' then 1 else 0 end as has_atat,case when dna_sequence regexp 'GGG' then 1 else 0 end as has_ggg from samples;
