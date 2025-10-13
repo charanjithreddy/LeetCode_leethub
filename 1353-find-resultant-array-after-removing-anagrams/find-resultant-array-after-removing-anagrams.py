50ea0ff8ed9c4114048413027287ed9c2e945b6d
charanjithreddy <charanjith.angadi@gmail.com>
2025-10-13 09:48:40 +0530

    Time: 43 ms (9.68%) | Memory: 12.5 MB (58.06%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def removeAnagrams(self, words):
         o=[];
-        for i in words:
-            t=[0]*26;
-            for j in i:
-                t[ord(j)-ord('a')]+=1;
-            o.append("".join([str(x) for x in t]));
-        
-        i=1;
+        i=0;
         while(i<len(words)):
-            if(o[i]==o[i-1]):
-                o.pop(i);
+            t=[0]*26;
+            for j in words[i]:
+                t[ord(j)-ord('a')]+=1;
+            if(len(o)==0):
+                o.append("".join([str(x) for x in t]));
+                i+=1;
+            elif("".join([str(x) for x in t])==o[-1]):
                 words.pop(i);
             else:
+                o.append("".join([str(x) for x in t]));
                 i+=1;
         return words;
         """
