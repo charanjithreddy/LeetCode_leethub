b02f1b1df7dada01a78f7d3820b7360fb246caa0
charanjithreddy <charanjith.angadi@gmail.com>
2025-11-09 10:39:52 +0530

    Time: 51 ms (52.87%) | Memory: 12.3 MB (83.33%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countOperations(self, num1, num2):
+        res=0;
+        while(num1!=0 and num2!=0):
+            if(num1>num2):
+                num1-=num2;
+            else:
+                num2-=num1;
+            res+=1;
+        return res;
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        
