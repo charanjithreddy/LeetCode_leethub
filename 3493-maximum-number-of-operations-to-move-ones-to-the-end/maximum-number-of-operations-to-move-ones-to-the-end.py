810d2ca5eed600cdebc7ab0cbd7fa4b168c7f81f
charanjithreddy <charanjith.angadi@gmail.com>
2025-11-13 15:07:21 +0530

    Time: 71 ms (84.09%) | Memory: 13 MB (70.45%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxOperations(self, s):
+        res=0;
+        ones=0;
+        flag=1;
+        for i in s:
+            if(i=='0'):
+                if(flag==1):
+                    res+=ones;
+                flag=0;
+            else:
+                ones+=1;
+                flag=1;
+        return res;
+        """
+        :type s: str
+        :rtype: int
+        """
+        
