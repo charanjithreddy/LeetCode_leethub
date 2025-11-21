0439e5a846fe6b229f3ad6ee3e921b5e9e4386b7
charanjithreddy <charanjith.angadi@gmail.com>
2025-11-21 10:17:36 +0530

    Time: 158 ms (76.00%) | Memory: 13.2 MB (48.00%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countPalindromicSubsequence(self, s):
+        first={};
+        last={};
+        for i in list(set(s)):
+            first[i]=s.index(i);
+            last[i]=len(s)-s[::-1].index(i)-1;
+        res=0;
+        for i in first:
+            res+=len(list(set(s[first[i]+1:last[i]])))
+        return res;
+
+        """
+        :type s: str
+        :rtype: int
+        """
+        
