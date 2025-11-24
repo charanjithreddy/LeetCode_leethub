cbe8883d64254610de73cc538b4b6a10905cd665
charanjithreddy <charanjith.angadi@gmail.com>
2025-11-24 17:16:27 +0530

    Time: 8 ms (60.87%) | Memory: 14.4 MB (6.09%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
@@ -2,11 +2,10 @@ class Solution(object):
     def prefixesDivBy5(self, nums):
         res=[nums[0]%5==0];
         for i in range(1,len(nums)):
-            nums[i]=nums[i-1]*2+nums[i];
+            nums[i]=(nums[i-1]*2+nums[i])%5;
             res.append(nums[i]%5==0);
         return res;
         """
         :type nums: List[int]
         :rtype: List[bool]
         """
-        
