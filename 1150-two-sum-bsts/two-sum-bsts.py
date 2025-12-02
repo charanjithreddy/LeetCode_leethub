d550361cd017701bd4a8feb5445af71f5100fd70
charanjithreddy <charanjith.angadi@gmail.com>
2025-12-02 17:28:38 +0530

    Time: 199 ms (14.29%) | Memory: 20.5 MB (57.14%) - LeetSync

---
+++ 1150-two-sum-bsts/two-sum-bsts.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def twoSumBSTs(self, root1, root2, target):
+        a1=[];
+        a2=[];
+        def func1(root):
+            if(root):
+                func1(root.left);
+                a1.append(root.val);
+                func1(root.right);
+        def func2(root):
+            if(root):
+                func2(root.left);
+                a2.append(root.val);
+                func2(root.right);
+        func1(root1);
+        func2(root2);
+        for i in a1:
+            if(target-i in a2):
+                return True;
+        return False;
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :type target: int
+        :rtype: bool
+        """
