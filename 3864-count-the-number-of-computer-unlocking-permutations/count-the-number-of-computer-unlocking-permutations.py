0e5472f3624cb88d2b473833db2c115f8b7919d9
charanjithreddy <charanjith.angadi@gmail.com>
2025-12-10 09:52:29 +0530

    Time: 4081 ms (11.11%) | Memory: 22.5 MB (66.67%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def countPermutations(self, complexity):
-        def fact(n):
-            if(n==0 or n==1):
-                return 1;
-            else:
-                return (n*fact(n-1))%(10**9+7);
         if(complexity[0]!=sorted(complexity)[1] and min(complexity)==complexity[0]):
-            return (fact(len(complexity)-1))%(10**9+7);
+            res=1;
+            for i in range(2,len(complexity)):
+                res*=i;
+            return res%(10**9+7);
         else:
             return 0;
         """
