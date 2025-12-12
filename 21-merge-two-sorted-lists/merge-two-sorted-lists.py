f9071d96bc0ca4c4defca299febaa4c0c5f83f8a
charanjithreddy <charanjith.angadi@gmail.com>
2025-12-12 18:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (72.19%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,41 +6,49 @@
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         if(list1==None):
-            if(list2==None):
-                return None;
-            else:
-                return list2;
+            return list2;
         elif(list2==None):
             return list1;
         else:
-            temp=None;
             if(list1.val<list2.val):
+                temp1=list1.next;
+                temp2=list2;
                 temp=list1;
-                list1=list1.next;
+                while(temp1!=None and temp2!=None):
+                    if(temp1.val<temp2.val):
+                        temp.next=temp1;
+                        temp1=temp1.next;
+                    else:
+                        temp.next=temp2;
+                        temp2=temp2.next;
+                    temp=temp.next;
+                if(temp1==None):
+                    temp.next=temp2;
+                if(temp2==None):
+                    temp.next=temp1;
+                return list1;
             else:
+                temp2=list2.next;
+                temp1=list1;
                 temp=list2;
-                list2=list2.next;
-            head=temp;
-            while(list1!=None and list2!=None):
-                if(list1.val<list2.val):
-                    temp.next=list1;
+                while(temp1!=None and temp2!=None):
+                    if(temp1.val<temp2.val):
+                        temp.next=temp1;
+                        temp1=temp1.next;
+                    else:
+                        temp.next=temp2;
+                        temp2=temp2.next;
                     temp=temp.next;
-                    list1=list1.next;
-                else:
-                    temp.next=list2;
-                    temp=temp.next;
-                    list2=list2.next;
-            while(list1):
-                temp.next=list1;
-                temp=temp.next;
-                list1=list1.next;
-            while(list2):
-                temp.next=list2;
-                temp=temp.next;
-                list2=list2.next;
-            return head;
+                if(temp1==None):
+                    temp.next=temp2;
+                if(temp2==None):
+                    temp.next=temp1;
+                return list2;
+
+
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        
