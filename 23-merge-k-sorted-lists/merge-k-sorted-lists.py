cc7ea0c2305aade9f81b047afcdb7fed2d86bef1
charanjithreddy <charanjith.angadi@gmail.com>
2025-12-12 21:30:19 +0530

    Time: 31 ms (22.59%) | Memory: 18.1 MB (68.96%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,51 +5,35 @@
 #         self.next = next
 class Solution(object):
     def mergeKLists(self, lists):
-
         def merge(list1,list2):
             if(list1==None):
                 return list2;
             elif(list2==None):
                 return list1;
             else:
+                head=None;
                 if(list1.val<list2.val):
-                    temp1=list1.next;
-                    temp2=list2;
-                    temp=list1;
-                    while(temp1!=None and temp2!=None):
-                        if(temp1.val<temp2.val):
-                            temp.next=temp1;
-                            temp1=temp1.next;
-                        else:
-                            temp.next=temp2;
-                            temp2=temp2.next;
-                        temp=temp.next;
-                    if(temp1!=None):
-                        temp.next=temp1;
-                    if(temp2!=None):
-                        temp.next=temp2;
-                    return list1;
+                    head=list1;
+                    list1=list1.next;
                 else:
-                    temp2=list2.next;
-                    temp1=list1;
-                    temp=list2;
-                    while(temp1!=None and temp2!=None):
-                        if(temp1.val<temp2.val):
-                            temp.next=temp1;
-                            temp1=temp1.next;
-                        else:
-                            temp.next=temp2;
-                            temp2=temp2.next;
-                        temp=temp.next;
-                    if(temp1!=None):
-                        temp.next=temp1;
-                    if(temp2!=None):
-                        temp.next=temp2;
-                    return list2;
+                    head=list2;
+                    list2=list2.next;
+                temp=head;
+                while(list1!=None and list2!=None):
+                    if(list1.val<list2.val):
+                        temp.next=list1;
+                        list1=list1.next;
+                    else:
+                        temp.next=list2;
+                        list2=list2.next;
+                    temp=temp.next;
+                if(list1!=None):
+                    temp.next=list1;
+                if(list2!=None):
+                    temp.next=list2;
+                return head;
         if(len(lists)==0):
             return None;
-        
-        
         while(len(lists)>1):
             temp=[];
             for i in range(0,len(lists),2):
@@ -63,4 +47,3 @@ class Solution(object):
         :type lists: List[Optional[ListNode]]
         :rtype: Optional[ListNode]
         """
-        
