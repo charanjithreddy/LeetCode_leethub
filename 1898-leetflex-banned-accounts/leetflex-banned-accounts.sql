b74e5ac25e5810df88fd03ec399f6f45cf167793
charanjithreddy <charanjith.angadi@gmail.com>
2026-01-13 10:01:15 +0530

    Time: 633 ms (9.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select distinct(l1.account_id) from loginfo l1  join loginfo l2 on (l1.account_id=l2.account_id and l1.ip_address<l2.ip_address ) where l2.login<=l1.login and l1.login<=l2.logout or l1.login<=l2.login and l2.login<=l1.logout
