eb44573133f30fbc8408d9d8470b562377476d8d
charanjithreddy <charanjith.angadi@gmail.com>
2026-01-20 09:27:32 +0530

    Time: 163 ms (25.00%) | Memory: 12.6 MB (25.00%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minBitwiseArray(self, nums):
+        res=[];
+        for i in nums:
+            flag=1;
+            for j in range(i):
+                if(j|(j+1)==i):
+                    res.append(j);
+                    flag=0;
+                    break;
+            if(flag==1):
+                res.append(-1);
+        return res;
+
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
