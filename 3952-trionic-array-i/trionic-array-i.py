9f499c82842e4eb8c1dfa754cdb0f2f1ccd3668c
charanjithreddy <charanjith.angadi@gmail.com>
2026-02-03 09:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.14%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isTrionic(self, nums):
+        p=0;
+        flag1,flag2,flag3=0,0,0;
+        while(p<len(nums)-1):
+            if(nums[p+1]>nums[p]):
+                flag1=1;
+                p+=1;
+            else:
+                break;
+        q=p;
+        while(q<len(nums)-1):
+            if(nums[q+1]<nums[q]):
+                flag2=1;
+                q+=1;
+            else:
+                break;
+        r=q;
+        while(r<len(nums)-1):
+            if(nums[r+1]>nums[r]):
+                flag3=1;
+                r+=1;
+            else:
+                break;
+        return r==len(nums)-1 and flag1==1 and flag2==1 and flag3==1;
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
