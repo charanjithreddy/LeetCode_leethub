9f14966f52fbc5989cc088b13909308faeb725db
charanjithreddy <charanjith.angadi@gmail.com>
2026-02-13 09:40:56 +0530

    Time: 115 ms (44.14%) | Memory: 17.9 MB (71.16%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -2,17 +2,12 @@ class Solution(object):
     def kClosest(self, points, k):
         arr=[];
         heapq.heapify(arr);
-        d={};
         for i in points:
             dist=i[0]*i[0]+i[1]*i[1]
-            if(dist in d):
-                d[dist].append(i);
-            else:
-                d[dist]=[i];
-            heapq.heappush(arr,dist);
+            heapq.heappush(arr,(dist,i));
         res=[];
         while(len(res)<k):
-            res.extend(d[heapq.heappop(arr)]);
+            res.append(heapq.heappop(arr)[1]);
         return res;
         """
         :type points: List[List[int]]
