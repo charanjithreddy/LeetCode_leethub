af8953ea69b8cd5e9ae80e5b037656889e713367
charanjithreddy <charanjith.angadi@gmail.com>
2026-02-21 10:17:03 +0530

    Time: 195 ms (51.27%) | Memory: 12.7 MB (54.82%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countPrimeSetBits(self, left, right):
+        def isprime(n):
+            if(n<=1):
+                return False
+            i=2;
+            while(i*i<=n):
+                if(n%i==0):
+                    return False;
+                i+=1;
+            return True
+        res=0;
+        for i in range(left,right+1):
+            if(isprime(bin(i)[2:].count("1"))):
+                res+=1;
+        return res;
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        
