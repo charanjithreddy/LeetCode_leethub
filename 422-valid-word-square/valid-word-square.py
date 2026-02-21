b3d3029605eb4c0da9086d7274a22c00e776acfe
charanjithreddy <charanjith.angadi@gmail.com>
2026-02-21 19:01:59 +0530

    Time: 19 ms (88.89%) | Memory: 13 MB (57.78%) - LeetSync

---
+++ 422-valid-word-square/valid-word-square.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def validWordSquare(self, words):
+        try:
+            for i in range(len(words)):
+                for j in range(len(words[i])):
+                    if(words[i][j]!=words[j][i]):
+                        return False;
+            return True;
+        
+        except IndexError as e:
+            return False;
+        """
+        :type words: List[str]
+        :rtype: bool
+        """
+        
