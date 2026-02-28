0ed6f410151dc413a88174ad05cea08534dcedfa
charanjithreddy <charanjith.angadi@gmail.com>
2026-02-28 15:06:34 +0530

    Time: 820 ms (37.50%) | Memory: 17.6 MB (70.83%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def concatenatedBinary(self, n):
+        res="";
+        for i in range(1,n+1):
+            res+=bin(i)[2:];
+        return int(res,2)%(10**9+7)
+        """
+        :type n: int
+        :rtype: int
+        """
+        
