b455420725353368f85ca57a2117ce8f86d7a8c4
charanjithreddy <charanjith.angadi@gmail.com>
2026-03-25 10:09:08 +0530

    Time: 1885 ms (9.09%) | Memory: 13.4 MB (36.36%) - LeetSync

--- 1903-design-most-recently-used-queue/design-most-recently-used-queue.py
+++ 1903-design-most-recently-used-queue/design-most-recently-used-queue.py
@@ -1,12 +1,9 @@
 class MRUQueue(object):
-
     def __init__(self, n):
         self.a=[i for i in range(1,n+1)]
         """
         :type n: int
         """
-        
-
     def fetch(self, k):
         t=self.a[k-1];
         for i in range(k-1,len(self.a)-1):
@@ -17,9 +14,6 @@ class MRUQueue(object):
         :type k: int
         :rtype: int
         """
-        
-
-
 # Your MRUQueue object will be instantiated and called as such:
 # obj = MRUQueue(n)
 # param_1 = obj.fetch(k)
