f5dfc556ba1132fdd67b41b712051bdf3c9db05a
charanjithreddy <charanjith.angadi@gmail.com>
2026-03-30 09:33:54 +0530

    Time: 258 ms (33.33%) | Memory: 16.8 MB (83.33%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def checkStrings(self, s1, s2):
-        s1_even=[];
-        s1_odd=[];
-        s2_even=[];
-        s2_odd=[]
+        s1_even=[0]*26;
+        s1_odd=[0]*26;
+        s2_even=[0]*26
+        s2_odd=[0]*26
         for i in range(len(s1)):
             if(i%2==0):
-                s1_even.append(s1[i]);
-                s2_even.append(s2[i]);
+                s1_even[ord(s1[i])-ord('a')]+=1
+                s2_even[ord(s2[i])-ord('a')]+=1
             else:
-                s1_odd.append(s1[i]);
-                s2_odd.append(s2[i]);
-        return sorted(s1_even)==sorted(s2_even) and sorted(s1_odd)==sorted(s2_odd)
-
+                s1_odd[ord(s1[i])-ord('a')]+=1
+                s2_odd[ord(s2[i])-ord('a')]+=1
+        return (s1_even)==(s2_even) and (s1_odd)==(s2_odd)
         """
         :type s1: str
         :type s2: str
         :rtype: bool
         """
-        
