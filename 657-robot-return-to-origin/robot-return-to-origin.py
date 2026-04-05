6a18bd90d7356320f5915e03ddbb93517e9d0650
charanjithreddy <charanjith.angadi@gmail.com>
2026-04-05 09:48:58 +0530

    Time: 54 ms (6.23%) | Memory: 12.5 MB (64.78%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def judgeCircle(self, moves):
         r=0;
-        c=0;
+        l=0;
+        u=0;
+        d=0;
         for i in moves:
             if(i=="R"):
-                c+=1;
-            if(i=="L"):
-                c-=1;
-            if(i=="U"):
                 r+=1;
+            if(i=="L"):
+                l+=1;
+            if(i=="U"):
+                u+=1;
             if(i=="D"):
-                r-=1;
-        return r==0 and c==0
+                d+=1
+        return r==l and u==d
         """
         :type moves: str
         :rtype: bool
