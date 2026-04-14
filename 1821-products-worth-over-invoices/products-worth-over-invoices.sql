14440699114c3c379f3d1749a1e31b369c713352
charanjithreddy <charanjith.angadi@gmail.com>
2026-04-14 10:32:41 +0530

    Time: 813 ms (30.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1821-products-worth-over-invoices/products-worth-over-invoices.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.name,ifnull(sum(rest),0) as rest,ifnull(sum(paid),0) as paid,ifnull(sum(canceled),0) as canceled,ifnull(sum(refunded),0) as refunded from product p left join invoice i on p.product_id=i.product_id group by p.product_id order by p.name
