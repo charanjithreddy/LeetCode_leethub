dd0a814213d7476aca26facba0f8e316cfdfffa2
charanjithreddy <charanjith.angadi@gmail.com>
2026-05-17 18:03:21 +0530

    Time: 2323 ms (14.99%) | Memory: 12.6 MB (66.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
-        a=[1]*len(nums);
-        res=1;
+        dp=[1]*len(nums);
         for i in range(len(nums)-2,-1,-1):
             for j in range(i+1,len(nums)):
                 if(nums[j]>nums[i]):
-                    a[i]=max(a[i],1+a[j]);
-            res=max(res,a[i]);
-        return res;
+                    dp[i]=max(dp[i],1+dp[j]);
+        return max(dp)
         """
         :type nums: List[int]
         :rtype: int
         """
+        
