4c45ea4c786d55f90af6c14574603b1e3ea23b6d
charanjithreddy <charanjith.angadi@gmail.com>
2026-05-17 16:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def climbStairs(self, n):
-        if(n<2):
-            return 1;
+        
         a=[0]*(n+1);
-        a[0]=0;
-        a[1]=1;
-        a[2]=2;
-        for i in range(3,n+1):
+        a[0]=1;
+        a[1]=2;
+        for i in range(2,n):
             a[i]=a[i-1]+a[i-2];
-        return a[n];
+        return a[n-1]
         """
         :type n: int
         :rtype: int
