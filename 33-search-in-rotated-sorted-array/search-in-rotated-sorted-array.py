b087b536252aa7909d3ed2055b08b935c16be390
charanjithreddy <charanjith.angadi@gmail.com>
2026-05-22 10:54:44 +0530

    Time: 2 ms (7.06%) | Memory: 12.6 MB (44.96%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -7,18 +7,19 @@ class Solution(object):
             if(nums[mid]==target):
                 return mid;
             elif(nums[mid]>=nums[l]):
-                if(target>nums[mid] or target<nums[l]):
-                    l=mid+1;
-                else:
-                    r=mid-1;
-            else:
-                if(target>nums[r] or target<nums[mid]):
+                if(nums[l]<=target<=nums[mid]):
                     r=mid-1;
                 else:
                     l=mid+1;
+            elif(nums[mid]<nums[r]):
+                if(nums[mid]<=target<=nums[r]):
+                    l=mid+1;
+                else:
+                    r=mid-1
         return -1;
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
+        
