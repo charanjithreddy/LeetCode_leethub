6d6f9ec176b267a4b41cc5ce8f045916e057ba59
charanjithreddy <charanjith.angadi@gmail.com>
2026-05-26 08:46:10 +0530

    Time: 2 ms (50.00%) | Memory: 12.3 MB (60.98%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -2,16 +2,17 @@ class Solution(object):
     def numberOfSpecialChars(self, word):
         small=[0]*26;
         big=[0]*26;
+        res=0;
         for i in word:
             if("a"<=i<="z"):
+                if(big[ord(i)-ord('a')]==1 and small[ord(i)-ord('a')]==0):
+                    res+=1;
                 small[ord(i)-ord('a')]=1;
             else:
+                if(small[ord(i)-ord('A')]==1 and big[ord(i)-ord('A')]==0):
+                    res+=1;
                 big[ord(i)-ord('A')]=1;
-        res=0;
-        for i in range(26):
-            if(small[i]==1 and big[i]==1):
-                res+=1;
-        return res
+        return res;
         """
         :type word: str
         :rtype: int
