1b6f3d696feb899204e7a43183cb6b0119a707ef
charanjithreddy <charanjith.angadi@gmail.com>
2026-06-05 21:44:59 +0530

    Time: 831 ms (28.07%) | Memory: 0B (100.00%) - LeetSync

--- 1162-project-employees-ii/project-employees-ii.sql
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select project_id from (
-select project_id,count(employee_id) as cnt from project group by project_id) as t where cnt in (
-select max(cnt) as max_cnt from (select count(employee_id) as cnt from project p group by project_id) as t)
+with cte as (select project_id,count(employee_id) as cnt from project group by project_id)
+select project_id from cte as t where cnt in (
+select max(cnt) as max_cnt from cte as t)
