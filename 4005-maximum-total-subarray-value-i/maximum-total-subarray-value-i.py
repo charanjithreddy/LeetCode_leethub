937edcce17a1d370b41a2096a46cb5286822e739
charanjithreddy <charanjith.angadi@gmail.com>
2026-06-09 15:30:12 +0530

    Time: 107 ms (5.88%) | Memory: 16.6 MB (0.00%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.py
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.py
@@ -1,6 +1,11 @@
 class Solution(object):
     def maxTotalValue(self, nums, k):
-        return k*(max(nums)-min(nums))
+        maxi=float("-inf");
+        mini=float("inf");
+        for i in nums:
+            maxi=max(maxi,i);
+            mini=min(mini,i)
+        return k*(maxi-mini)
         """
         :type nums: List[int]
         :type k: int
