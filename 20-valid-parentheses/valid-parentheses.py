5a934ee469d88868d3e5f084cf21a304e4a0ce9b
charanjithreddy <charanjith.angadi@gmail.com>
2026-09-16 15:25:37 +0530

    Time: 4 ms (40.92%) | Memory: 12.6 MB (11.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,25 +1,23 @@
 class Solution(object):
     def isValid(self, s):
         stack=[];
-        l=0;
         for i in s:
-            if(i=='(' or i=='{' or i=='['):
+            if(i in ["(","{","["]):
                 stack.append(i);
-                l+=1;
             else:
-                if(i==')' and l>0 and stack[l-1]=='('):
-                    stack.pop(l-1);
-                    l-=1;
-                elif(i==']' and l>0  and stack[l-1]=='['):
-                    stack.pop(l-1);
-                    l-=1;
-                elif(i=='}' and l>0  and stack[l-1]=='{'):
-                    stack.pop(l-1);
-                    l-=1;
+                if(len(stack)==0):
+                    return False
+                if(i==")" and stack and stack[-1]=="("):
+                    stack.pop(-1);
+                elif(i=="]" and stack and stack[-1]=="["):
+                    stack.pop(-1);
+                elif(i=="}" and stack and stack[-1]=="{"):
+                    stack.pop(-1);
                 else:
-                    return False;
-        return len(stack)==0;
+                    break;
+        return len(stack)==0
         """
         :type s: str
         :rtype: bool
         """
+        
